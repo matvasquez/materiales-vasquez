dccf79a2b6887bed12d95e91bc4b88a3284520e3
PonceGL <poncianogl@hotmail.com>
2021-10-26 16:29:26 -0500

    Enter disable

--- components/SearchResults/SearchResults.js
+++ components/SearchResults/SearchResults.js
@@ -19,24 +19,24 @@ const SearchResults = ({
   clear,
   searchName,
 }) => {
-  const handleEnter = (e) => {
-    if (e.key === "Enter") {
-      if (searchResults.length === 8) {
-        clear();
-        window.location.href = `${window.location.origin}/todos-los/${searchName}`;
-      } else {
-        alert("No hay suficientes artículos para verlos en una página nueva ");
-      }
-    }
-  };
-  useEffect(() => {
-    if (searchResults.length > 0) {
-      window.addEventListener("keypress", (e) => handleEnter(e));
-    }
-    return () => {
-      window.removeEventListener("keypress", (e) => handleEnter(e));
-    };
-  }, [searchResults, searchName]);
+  // const handleEnter = (e) => {
+  //   if (e.key === "Enter") {
+  //     if (searchResults.length === 8) {
+  //       clear();
+  //       window.location.href = `${window.location.origin}/todos-los/${searchName}`;
+  //     } else {
+  //       alert("No hay suficientes artículos para verlos en una página nueva ");
+  //     }
+  //   }
+  // };
+  // useEffect(() => {
+  //   if (searchResults.length > 0) {
+  //     window.addEventListener("keypress", (e) => handleEnter(e));
+  //   }
+  //   return () => {
+  //     window.removeEventListener("keypress", (e) => handleEnter(e));
+  //   };
+  // }, [searchResults, searchName]);
 
   useEffect(() => {
     window.innerWidth < 1100 && window.scrollTo({ top: 0, behavior: "smooth" });
