390bd4dccade909dcc6b1ec1e087435d5e19adb4
PonceGL <poncianogl@hotmail.com>
2021-12-06 10:30:16 -0600

    update api/todos-los-articulos

--- pages/api/todos-los-articulos.js
+++ pages/api/todos-los-articulos.js
@@ -19,21 +19,22 @@ export default async function getProductsByCategorie(req, res) {
       .json({ message: "Lo sentimos, sólo aceptamos solicitudes GET" });
   }
 
-  const query = `SELECT RTRIM(a.CLAVEART) AS articulo_id, RTRIM(a.DESC_BREVE) AS name, RTRIM(a.DESCRIBEAR) AS description, l.PREC_IVA1 AS price, RTRIM(g.DESGIR) AS category, RTRIM(g2.DESC_GIR2) AS main_category
+  const query = `SELECT RTRIM(a.CLAVEART) AS articulo_id, RTRIM(a.DESCRIBEAR) AS description, RTRIM(l.PREC_IVA1) AS price, RTRIM(g.DESGIR) AS category, RTRIM(g2.DESC_GIR2) AS main_category, RTRIM(m.DESC_MARCA) AS brand
   FROM ARTICULO AS a
-  LEFT OUTER JOIN ARTLISTA AS l
-      ON a.CLAVEART = l.CLAVEART
-  LEFT OUTER JOIN ARTGIRO AS g
-      ON a.CLAVEGIR = g.CLAVEGIR
-  LEFT OUTER JOIN ARTGIRO2 AS g2
-      ON a.CLAVEGIR2 = g2.CLAVEGIR2
-  LEFT OUTER JOIN IMAGENES AS i
-      ON a.CLAVEART = i.CAMPO1
-  LEFT OUTER JOIN ART_ALM AS s
-      ON a.CLAVEART = s.CLAVEART
-  WHERE a.HABVTAS = '' AND l.NO_LISTAP = '001' AND i.IMAGEN IS NOT NULL AND i.SELECTO = 'S' AND s.CVEALM IN ('0020','0007','0018','0014','0015','0002','0008','0023','0017','0028','0027', '0021')
-  GROUP BY a.CLAVEART, a.DESC_BREVE, a.DESCRIBEAR, l.PREC_IVA1, g.DESGIR, a.CLAVEGIR, g2.DESC_GIR2, i.IMAGEN, a.FECHA_ALTA, i.SELECTO
-  ORDER BY a.FECHA_ALTA DESC`;
+LEFT OUTER JOIN ARTLISTA AS l
+    ON a.CLAVEART = l.CLAVEART
+LEFT OUTER JOIN ARTGIRO AS g
+    ON a.CLAVEGIR = g.CLAVEGIR
+LEFT OUTER JOIN ARTGIRO2 AS g2
+    ON a.CLAVEGIR2 = g2.CLAVEGIR2
+LEFT OUTER JOIN ARTICULO_IMG AS i
+    ON a.CLAVEART = i.CLAVEART
+LEFT OUTER JOIN ART_ALM AS s
+    ON a.CLAVEART = s.CLAVEART
+LEFT OUTER JOIN MARCAS AS m
+    ON a.CVE_MARCA = m.CVE_MARCA
+WHERE a.HABVTAS = '' AND l.NO_LISTAP = '001' AND i.IMAGEN IS NOT NULL AND i.SELECTO = 'S' AND s.CVEALM IN ('0020','0007','0018','0014','0015','0002','0008','0023','0017','0028','0027', '0021')
+GROUP BY a.CLAVEART, a.DESC_BREVE, a.DESCRIBEAR, l.PREC_IVA1, g.DESGIR, a.CLAVEGIR, g2.DESC_GIR2, i.IMAGEN, a.FECHA_ALTA, m.DESC_MARCA`;
 
   setTimeout(async () => {
     const result = await rest.executeQuery(query);
@@ -41,7 +42,6 @@ export default async function getProductsByCategorie(req, res) {
     result &&
       res.status(200).json({
         name: `Todos los Productos`,
-        method: req.method,
         total: result.data[0].length,
         data: result.data[0],
       });
